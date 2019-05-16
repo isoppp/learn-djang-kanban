9b8b8e6186527cd41667cff69abef9800293064f
isoppp <prog@d2deck.com>
2019-05-16 23:13:25 +0900

    Set config for board app

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "kanban.board.apps.BoardConfig",
 ]
 
 ROOT_URLCONF = "config.urls"
