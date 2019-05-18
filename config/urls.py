37d069529feb441c011a76fa7830a1b7b44d3abe
isoppp <prog@d2deck.com>
2019-05-18 22:03:44 +0900

    Set router for ticket api

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [path("admin/", admin.site.urls)]
+from kanban.board.views import TicketViewSet
+
+router = DefaultRouter()
+router.register("ticket", TicketViewSet)
+
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router.urls))]
