618b2e50722cc51a4feb512562f42994cd0a9d16
isoppp <prog@d2deck.com>
2019-05-19 13:34:32 +0900

    Add GET test with TicketFactory

--- kanban/board/tests/test_ticket.py
+++ kanban/board/tests/test_ticket.py
@@ -7,8 +7,9 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from faker import Faker
 
-from kanban.board.constants import TicketStatus
-from kanban.board.models import Ticket
+from .factories import TicketFactory
+from ..constants import TicketStatus
+from ..models import Ticket
 
 
 class TicketTests(TestCase):
@@ -67,3 +68,23 @@ class TicketTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Ticket.objects.count(), 1)
         self.assertEqual(Ticket.objects.get().name, payload["name"])
+
+    def test_get(self):
+        ticket = TicketFactory()
+        url = reverse("ticket-detail", kwargs={"pk": ticket.id})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {
+                "id": ticket.id,
+                "assignee": ticket.assignee.username,
+                "status_display": ticket.get_status_display(),
+                "name": ticket.name,
+                "description": ticket.description,
+                "status": ticket.status,
+                "start": ticket.start,
+                "end": ticket.end,
+            },
+        )
