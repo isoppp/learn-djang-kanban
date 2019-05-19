2db2bbaffd8ff71dc2b4d1a5990495c0865abf8f
isoppp <prog@d2deck.com>
2019-05-19 12:29:39 +0900

    Add same dates test

--- kanban/board/tests/test_ticket.py
+++ kanban/board/tests/test_ticket.py
@@ -49,3 +49,21 @@ class TicketTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Ticket.objects.count(), 0)
         self.assertEqual(response.data, {"non_field_errors": ["'end' value must set after 'start' day"]})
+
+    def test_validate_same_dates(self):
+        url = reverse("ticket-list")
+        date = str(self.fake.date_object())
+        payload = {
+            "assignee": None,
+            "name": self.fake.word(),
+            "description": self.fake.text(),
+            "status": random.choice([x.value for x in TicketStatus]),
+            "start": date,
+            "end": date,
+        }
+
+        response = self.client.post(url, json.dumps(payload), content_type="application/json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Ticket.objects.count(), 1)
+        self.assertEqual(Ticket.objects.get().name, payload["name"])
