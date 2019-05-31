56caff05ef14d51cff67a1f9444afb3091537e21
isoppp <prog@d2deck.com>
2019-05-31 19:52:33 +0800

    Add kanban UI

--- static/src/app/app.component.ts
+++ static/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  todo = [
+    'キャプチャ画像',
+    'レビュー依頼',
+  ]
+  inprogress = [
+    'サンプルコード実装',
+    '推敲',
+  ]
+  done = [
+    '現行執筆',
+  ]
   title = 'static';
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      )
+    }
+  }
 }
